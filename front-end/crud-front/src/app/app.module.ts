180c9bff711447982e6e3c2e97dda17648033ef2
Bruno Knorre Quadros <brunoknorre@gmail.com>
2024-02-19 18:35:37 -0300

    folders

--- front-end/crud-front/src/app/app.module.ts
+++ front-end/crud-front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCustomerComponent } from './components/post-customer/post-customer.component';
+import { GetAllCustomerComponent } from './components/get-all-customer/get-all-customer.component';
+import { UpdateCustomerComponent } from './components/update-customer/update-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCustomerComponent,
+    GetAllCustomerComponent,
+    UpdateCustomerComponent
   ],
   imports: [
     BrowserModule,
